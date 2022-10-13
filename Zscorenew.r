b919afc9d39b79877a49bf03ac64c02e1f0e7f2c
arkajparkhe123 <114654479+arkajparkhe123@users.noreply.github.com>
2022-10-13 23:40:35 +0530

    Update Zscorenew.r

--- Zscorenew.r
+++ Zscorenew.r
@@ -1,20 +1,24 @@
-library(matrixStats)
+#Import the required libraries
+library(matrixStats) 
 library(ComplexHeatmap)
+
+#Create a function named Heat that will calculate the z score
+Heat=function(var){ 
+  for(i in 1:nrow(var)){
+    z_score = (var - rowMeans(var))/rowSds(as.matrix(var))[row(var)]
+  }
+  
+  z_score[is.na(z_score)]=0  #na values to 0
+  zcs = as.matrix(z_score)  #convert to matrix
+  return(Heatmap(zcs[1:10]))  #return the Heatmap of the matrix zcs 
+} 
+
 
 can=readRDS('logCPM.rds')
 
-function2=function(can){ 
-  for(i in 1:nrow(can)){
-    z_score = (can - rowMeans(can))/rowSds(as.matrix(can))[row(can)]
-  }
-  
-  z_score[is.na(z_score)]=0 
-  zcs = as.matrix(z_score) 
-  return(Heatmap(zcs[1:10])) 
-} 
-function2(can)
+Heat(can) #Input can variable in the Heat function and it will return the Heatmap of the dataset
 
-pdf('ZScore.pdf',width = 10,height = 10)
+pdf('ZScore.pdf',width = 10,height = 10) #Save the Heatmap in the form of pdf
 
 plot(1:5,pch=20)
 
