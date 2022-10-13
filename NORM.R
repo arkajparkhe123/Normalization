f7b707490aa236292e5d6805f95265b3f444ebdf
arkajparkhe123 <114654479+arkajparkhe123@users.noreply.github.com>
2022-10-13 22:33:38 +0530

    Update NORM.R

--- NORM.R
+++ NORM.R
@@ -1,31 +1,29 @@
 #Reading an input cancer data file
 data=read.csv("C:/Users/arkaj/OneDrive/Documents/gene2.csv",sep=",",header=T,row.names = 1)
-dim(data)
-#Create a count per matrix
+dim(data) #check the dimension of the dataset
+#Create a count per matrix of the whole dataset
 cpm = data
 for(i in 1:ncol(data)){
   cpm[,i]=(data[,i]/sum(data[,i]))*1000000
 }
-#Calculate a log of cpm
+#Calculate the log value of cpm 
 logcpm=log2(cpm+1)
 logcpm
 saveRDS(logcpm,file="logCPM.rds")
 summary(logcpm)
 
 
-#list()
-#rm(list = ls())
-#Calculate a z score
-library(matrixStats)
-z_score = (logcpm - rowMeans(logcpm))/rowSds(as.matrix(logcpm))[row(logcpm)]
-z_score
+#Calculate the z score
+library(matrixStats) #Import MatrixStats for calculating z score
+zscr = (logcpm - rowMeans(logcpm))/rowSds(as.matrix(logcpm))[row(logcpm)]
+zscr
 
 
 #Calculate variance using log 
 variance = apply(logcpm, 1, var)
 variance = sort(variance,decreasing = T)
 top50 = variance[1:50]
-map = z_score[names(top50),]
+map = zscr[names(top50),]
 
-library(ComplexHeatmap)
+library(ComplexHeatmap) #Import ComplexHeatmap to plot the Heatmap of the z score values
 Heatmap(map)
